f21dcbb511c457f4f17277d1e12d5565503ae954
andrewcrobertson <andrewcrobertson@users.noreply.github.com>
2021-05-10 09:27:57 +0000

    deploy: ba005ab92570bc53728bb9571162c9f0f3e10420

---
+++ fixture.ts
@@ -0,0 +1,10 @@
+import * as fs from "fs";
+import { getSeasonPostContent } from "./src/getSeasonPostContent";
+
+(async () => {
+  const postContent = await getSeasonPostContent();
+  fs.writeFileSync(
+    `index.html`,
+    Buffer.from(`\ufeff${postContent}`, "utf16le")
+  );
+})();
